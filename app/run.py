613ee02f0296e490ea20e2e061edd4e71ae69381
Grant Orndorff <grant@orndorff.me>
2025-09-15 18:08:52 -0400

    load grades from json

--- app/run.py
+++ app/run.py
@@ -1,4 +1,5 @@
-
+import json
+import sys
 
 from grades import Grades
 from grade_weights import GradeWeights
@@ -6,12 +7,15 @@ from grade_calculator import GradeCalculator
 
 # This runs the grade calculation.
 
-# Instatiate Grade and Weights objects
-my_grades = Grades()
-weights = GradeWeights()
+# Use the first arg as the json file
+json_file_path = sys.argv[1]
 
-# Set grades achieved so far
-my_grades.quiz_1 = 0.78 # Received 78% in the first quiz
+with open(json_file_path, "r") as json_file:
+    grades_dict = json.load(json_file)
+
+# Instatiate Grade and Weights objects
+my_grades = Grades(**grades_dict)
+weights = GradeWeights()
 
 # Print out the grades to console
 print(my_grades)
@@ -28,4 +32,3 @@ else:
 optimistic_percentage_grade = GradeCalculator.calculate_optimistic_course_percentage(my_grades, weights)
 optimistic_letter_grade = GradeCalculator.calculate_letter_grade(optimistic_percentage_grade)
 print(f'If all other assignments are 100%, the overall course would be {optimistic_percentage_grade*100}%, which is a {optimistic_letter_grade}')
-
